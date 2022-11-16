0e9f32fed91de763564f4df3e02294257c79278c
RubenChet <rubenchetboun@gmail.com>
2022-11-16 13:53:55 +0100

    my_app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">Hello</div>
+  <div class="Container">
+    <div>
+    <HelloWorld msg="Welcome to Your Vue.js App" /></div>
+    <Map />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Map,
+  }
+}
+</script>
+
+<style>
+.Container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: 3px solid black;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// add this
+import './index.css'
+import 'flowbite';
+
+createApp(App).mount('#app')
