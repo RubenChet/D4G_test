3990b8577609bee9c821adbb6add27c10baff2b2
RubenChet <rubenchetboun@gmail.com>
2022-11-18 02:04:47 +0100

    inshallah

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    publicPath: ''
-}
+  publicPath: "",
+};
+
+module.exports = {
+  configureWebpack: {
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      },
+    },
+  },
+};
